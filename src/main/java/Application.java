3d2e444c1443cb89aea5f68ffc0ba0d3d99f8ec3
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2020-05-27 14:54:53 +0100

    Create applicationContext.xml file and define beans using setter injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,13 @@
 import com.conference.service.SpeakerService;
 import com.conference.service.SpeakerServiceImplementation;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService speakerService = new SpeakerServiceImplementation();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService speakerService = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(speakerService.findAll().get(0).getFirstName());
         System.out.println(speakerService.findAll().get(0).getLastName());
--- src/main/java/com/conference/service/SpeakerServiceImplementation.java
+++ src/main/java/com/conference/service/SpeakerServiceImplementation.java
@@ -1,17 +1,20 @@
 package com.conference.service;
 
 import com.conference.model.Speaker;
-import com.conference.repository.HibernateSpeakerRepositoryImplementation;
 import com.conference.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImplementation implements SpeakerService {
 
-    private SpeakerRepository speakerRepository = new HibernateSpeakerRepositoryImplementation();
+    private SpeakerRepository speakerRepository;
 
     @Override
     public List<Speaker> findAll() {
         return speakerRepository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
     }
 }
